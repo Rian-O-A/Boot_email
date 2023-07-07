160c9a778d3760101fcf8ee2f91700bfc4f4be7c
Rian Almeida <rianoliveira38@gmail.com>
2023-07-07 12:34:38 -0300

    org

--- backend/auth/route.py
+++ backend/auth/route.py
@@ -2,8 +2,8 @@ from flask import request, jsonify, Blueprint
 
 auth_route = Blueprint(__name__, "auth")
 
-@auth_route.route("search", methods=["POST"])
+@auth_route.route("/search", methods=["POST"])
 def search():
     data = request.get_json()
     
-    pass
+    
