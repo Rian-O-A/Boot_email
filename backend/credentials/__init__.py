3cab6a50334efcbea788cf65227cc99e8c9d1567
Rian Oliveira <rianoliveira38@gmail.com>
2023-10-25 21:00:05 -0300

    Update  __init__.py

--- backend/credentials/__init__.py
+++ backend/credentials/__init__.py
@@ -4,16 +4,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 cred = {
-    "type": os.environ["type"],
-    "project_id": os.environ["project_id"],
-    "private_key_id": os.environ["private_key_id"],
-    "private_key": os.environ["private_key"].replace(r"\n", "\n"),
-    "client_email": os.environ["client_email"],
-    "client_id": os.environ["client_id"],
-    "auth_uri": os.environ["auth_uri"],
-    "token_uri": os.environ["token_uri"],
-    "auth_provider_x509_cert_url": os.environ["auth_provider_x509_cert_url"],
-    "client_x509_cert_url": os.environ["client_x509_cert_url"],
-    "universe_domain": os.environ["universe_domain"]
+    "type": os.environ["FIREBASE_TYPE"],
+    "project_id": os.environ["FIREBASE_PROJECT_ID"],
+    "private_key_id": os.environ["FIREBASE_PRIVATE_KEY_ID"],
+    "private_key": os.environ["FIREBASE_PRIVATE_KEY"].replace(r"\n", "\n"),
+    "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
+    "client_id": os.environ["FIREBASE_CLIENT_ID"],
+    "auth_uri": os.environ["FIREBASE_AUTH_URI"],
+    "token_uri": os.environ["FIREBASE_TOKEN_URI"],
+    "auth_provider_x509_cert_url": os.environ["FIREBASE_AUTH_PROVIDER_X509_CERT_URL"],
+    "client_x509_cert_url": os.environ["FIREBASE_CLIENT_X509_CERT_URL"],
+    "universe_domain": os.environ["FIREBASE_UNIVERSE_DOMAIN"]
 }
 
